ea09bd6c3a96a87c748e36dd6f485a9956554865
jsurtees <jsurtees9@gmail.com>
2017-11-12 00:31:12 -0500

    lots of updates
    new nav, new splash, still working on small screens

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "35%";
     document.getElementById("main").style.marginRight = "35%";
-    document.getElementById("start").style.height = "10em";
-    document.getElementById("biobody").style.fontSize = "0.6em";
-    document.getElementById("bioheader").style.fontSize = "1.5em";
-    document.getElementById("space").style.height = "22em";
-    document.getElementById("fill").style.background = "#494138";
-    document.getElementById("fill").style.borderLeft = "2px solid #EBBE50";
-    document.getElementById("fill").style.borderRight = "2px solid #EBBE50";
-    document.getElementById("fill").style.borderBottom = "2px solid #EBBE50";
-    document.getElementById("fill").style.transition = "0.3s ease";
+    // document.getElementById("start").style.height = "10em";
+    // document.getElementById("biobody").style.fontSize = "0.6em";
+    // document.getElementById("bioheader").style.fontSize = "1.5em";
+    // document.getElementById("space").style.height = "22em";
+    // document.getElementById("fill").style.background = "#494138";
+    // document.getElementById("fill").style.borderLeft = "2px solid #EBBE50";
+    // document.getElementById("fill").style.borderRight = "2px solid #EBBE50";
+    // document.getElementById("fill").style.borderBottom = "2px solid #EBBE50";
+    // document.getElementById("fill").style.transition = "0.3s ease";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
-    document.getElementById("start").style.height = "30em";
-    document.getElementById("biobody").style.fontSize = "1.2em";
-    document.getElementById("bioheader").style.fontSize = "2.5em";
-    document.getElementById("space").style.height = "30em";
-    document.getElementById("fill").style.background = '#494138';
-    document.getElementById("fill").style.borderLeft = "1px solid #EBBE50";
-    document.getElementById("fill").style.borderRight = "1px solid #EBBE50";
-    document.getElementById("fill").style.borderBottom = "1px solid #EBBE50";
-    document.getElementById("fill").style.transition = "0.3s ease";
+    // document.getElementById("start").style.height = "30em";
+    // document.getElementById("biobody").style.fontSize = "1.2em";
+    // document.getElementById("bioheader").style.fontSize = "2.5em";
+    // document.getElementById("space").style.height = "30em";
+    // document.getElementById("fill").style.background = '#494138';
+    // document.getElementById("fill").style.borderLeft = "1px solid #EBBE50";
+    // document.getElementById("fill").style.borderRight = "1px solid #EBBE50";
+    // document.getElementById("fill").style.borderBottom = "1px solid #EBBE50";
+    // document.getElementById("fill").style.transition = "0.3s ease";
 }
 
 // $(document).ready(function(){
@@ -43,16 +43,16 @@ $(document).ready(function(){
 	});
 });
 
-const tilt = $('[data-tilt]').tilt();
-let tiltResetTimeout;
-
-$('[data-tilt]').on('mousemove', function(e){
-  clearTimeout(tiltResetTimeout);
-
-  tiltResetTimeout = setTimeout(function(){
-    tilt.tilt.reset.call(tilt);
-  }, 2000)
-});
+// const tilt = $('[data-tilt]').tilt();
+// let tiltResetTimeout;
+//
+// $('[data-tilt]').on('mousemove', function(e){
+//   clearTimeout(tiltResetTimeout);
+//
+//   tiltResetTimeout = setTimeout(function(){
+//     tilt.tilt.reset.call(tilt);
+//   }, 2000)
+// });
 
 
 //
